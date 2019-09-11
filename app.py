43227d9ec2295ab49f48653952deee412037866d
Victor Ay <viktoray007@gmail.com>
2019-09-11 21:17:26 +0300

    mock up front-end

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from classifier.predict import predict
+import wget
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+
+    if request.method == 'POST':
+        print(request.form)
+        model = request.form['model']
+        gpu = True if request.form.get('gpu') else False
+        url = request.form['image']
+        image_path = download(url)
+
+        print(model, gpu, image_path)
+
     return render_template('index.html')
+
+
+def download(url):
+    name = url.rsplit('/')
+    path = f'images/{name[-1]}'
+    wget.download(url, path)
+    return path
 
 
 if __name__ == '__main__':
